1b204c38f6d03b27e27d8889ece90f50caed0032
akmaevvs <88401882+akmaevvs@users.noreply.github.com>
2023-09-15 22:49:15 +0400

    Update index.js

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ app.use(bodyParser.json()); // for parsing application/json
 app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
 
 // Константы
-const host = "garantex.io"; // для тестового сервера используйте stage.garantex.biz
+const host = "garantex.org"; // для тестового сервера используйте stage.garantex.biz
 // const host = "stage.garantex.biz"; // для тестового сервера используйте stage.garantex.biz
 
 // Методы
